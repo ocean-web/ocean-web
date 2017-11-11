16382bfe3c9aaa9fc521b4e52f33a0aa19aad3a6
moustafaelsisy <spaceflyby@gmail.com>
2017-11-11 15:13:07 -0330

    Test to check socket is working

--- socketHandler.js
+++ socketHandler.js
@@ -1,5 +1,8 @@
 const socketHandler = (client) => {
   client.on('location-changed', (data) => {
     console.log('location changed!')
+    console.log(data)
   })
 }
+
+module.exports = socketHandler
