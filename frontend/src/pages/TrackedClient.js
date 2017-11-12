2d00c2a8cc1b1cd497e7c1c2fa7092c6c7630857
Tomisin <tomisinjenrola@gmail.com>
2017-11-12 12:05:52 -0330

    Started creating selector for clients

--- frontend/src/pages/TrackedClient.js
+++ frontend/src/pages/TrackedClient.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import Select from 'react-select'
+
+const options = [
+    { value: 'one', label: 'Buoy 1'},
+    { value: 'two', label: 'Buoy 2'},
+    { value: 'three', label: 'Ship'},
+    { value: 'four', label: 'Rig'},
+];
+
+function logChange(val) {
+  console.log('Selected: ' + val);
+}
 
 class TrackedClient extends React.Component{
   componentDidMount(){
     this.props.socket.emit('location-changed', "Works!")
   }
+
   render(){
     return(
-      <div>Hello World</div>
+      <div>
+        <h3>Select a client</h3>
+        <Select
+          name="select-client"
+          value="one"
+          options={options}
+          onChange={logChange}
+        />
+      </div>
     )
   }
 }
